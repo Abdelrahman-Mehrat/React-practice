e286f9385d3cb5c98199959646bc3451eba514b9
abdo <abdo@info.com>
2022-06-25 05:01:02 -0700

    Modify json/add styles

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,21 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import UseFetch from "./useFetch";
-
 const BlogDetails = () => {
-    const ourId = useParams().id   ;
-     console.log(ourId);
-     const {data:blog,error,isPending} = UseFetch(`http://localhost:3000/blogs/${ourId}`)
-    return ( 
-        <div className="blog-details">
-            <h2>Blog details {ourId}</h2>
-            {isPending && <div>loading...</div> }
-            {error && <div>{error}</div>}
-            {blog && (
-                <article><h2> {blog.title}</h2> <p>Written by {blog.author}</p> <div>{blog.body}</div> </article>
-            )}
-   
-        </div>
-     );
-}
- 
+  const ourId = useParams().id;
+  const history = useHistory();
+  console.log(ourId);
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = UseFetch(`http://localhost:8000/blogs/${ourId}`);
+  const handleDelete = () => {
+    UseFetch(`http://localhost:8000/blogs/${ourId}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+  return (
+    <div className="blog-details">
+      <h2>Blog details {ourId}</h2>
+      {isPending && <div>loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2> {blog.title}</h2> <p>Written by {blog.author}</p>{" "}
+          <div>{blog.body}</div> <button onClick={handleDelete}></button>
+        </article>
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,23 @@
 import BlogList from "./BlogList";
 import UseFetch from "./useFetch";
 const Home = () => {
-      const {data,isPending,error,handleDelete} = UseFetch("http://localhost:3000/blogs")
-      console.log(data);
-    return ( 
-        <div className="home"> 
-        {error && <div>{error}</div>}
-        {isPending && <div>Loading...</div> }
-        {data.length !=0 && <BlogList blogs={data} handleDelete={handleDelete} title={"All Blogs"}/>}
-        </div>
-     );
-}
- 
+  const { data, isPending, error, handleDelete } = UseFetch(
+    "http://localhost:8000/blogs"
+  );
+  console.log(data);
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data.length != 0 && (
+        <BlogList
+          blogs={data}
+          handleDelete={handleDelete}
+          title={"All Blogs"}
+        />
+      )}
+    </div>
+  );
+};
+
 export default Home;
